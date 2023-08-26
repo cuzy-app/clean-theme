07f201512a12719962b2f56ef9a8c08a6454c401
Felix Hahn <info@hahn-felix.de>
2023-08-26 21:30:43 +0200

    clean-base (dark)

---
+++ build-dark-css.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# 1) clean-base (dark)
+cd themes/clean-base
+lessc less/dark/build.less css/dark.css
+
+# Note: Unfortunately the color extractor removes the CSS variables
+css-color-extractor css/dark.css css/dark.css --format=css
+
+# Re-add CSS variables and compress CSS
+cp css/dark.css css/temporary.less
+lessc less/dark/build2.less css/dark.css --clean-css="--s1 --advanced" --source-map=css/dark.css.map
+rm css/temporary.less
+
+# 2) clean-bordered (dark)
+
+# 3) clean-contrasted (dark)
