526358c55e7ae73a3b23a00f04bf1901f055a57e
Felix Hahn <info@hahn-felix.de>
2024-07-26 21:17:11 +0200

    temporary fix dark CSS

--- build-modified-less-and-css.sh
+++ build-modified-less-and-css.sh
@@ -8,7 +8,5 @@ php yii clean-theme/build-modified-sources
 # Compile CSS
 cd modules/clean-theme/themes/Clean
 lessc less/build.less css/theme.css --clean-css="--s1 --advanced" --source-map=css/theme.css.map
-# Compile dark CSS for the Dark mode module: https://github.com/felixhahnweilheim/humhub-dark-mode
-lessc less/dark/build.less css/dark.css --clean-css="--s1 --advanced"
 
 echo "CSS compiled. Save module configuration to generated humhub.clean-theme.dynamic.css file"
